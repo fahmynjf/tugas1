bef98b400322aa8a8308145bc232d8eeee7d560a
fahmynjf <114680424+fahmynjf@users.noreply.github.com>
2022-09-29 23:56:04 +0700

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+     <meta charset="UTF-8">
+	 <title> Soal UTS </title>
+	     <style>
+		    h1{
+			  color: white;
+			  text-align: center;
+		}
+            h3{
+              color: white;
+			  font-family: arial;
+              padding-right: 150px;
+              padding-left: 10px;
+        }
+            konten{
+              border-collapse: collapse;
+              width: 50px;
+              border: 1px solid white;
+              margin-top: 50px;
+        }
+            img{
+              padding-top: 20px;
+              padding-right: 35px;
+              padding-bottom: 10px;
+              padding-left: 23px;
+        }
+        </style>
+</head>
+<body style="background-color: grey;">
+        
+      <h1> </h1>
+	  <table class="konten">
+	  <tr>
+         <img src="index " width="auto">	  
+	     <img src="index1 " width="auto">	  
+	     <img src="index2 " width="auto">	  
+	     
+      </tr>
+	  <tr>
+	      <th><h3 style="padding:20px;padding-right:150px;"> Fresh </h1></th>
+		  <th><h3 style="padding-right:135px;"> Global </h3></th>
+		  <th><h3 style="padding-right:95px;"> Moodboster </h3></th>
+          
+	  </tr>
+	   <tr>
+	      <th><h3 style="padding:20px;padding-right:150px;"> Fresh </h1></th>
+		  <th><h3 style="padding-right:135px;"> Global </h3></th>
+		  <th><h3 style="padding-right:95px;"> Moodboster </h3></th>
+         
+	  </tr>
+</table>
+</body>
+</html>
